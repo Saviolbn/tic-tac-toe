6a682df589de7dd4af771578e7eb4cca70374c27
Saviolbn <125612304+Saviolbn@users.noreply.github.com>
2023-07-06 18:14:15 -0300

    css corrigido, novamente

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 import "./ScoreBoard.css"
 
-export const ScoreBoard = ({scores, playing}) => {
-    const { xScore, oScore } = scores;
-    return (
-        <div className={`scoreboard ${playing}`}>
-            <span>X - {xScore}</span>
-            <span>O - {oScore}</span>
+export const ScoreBoard = ({ scores, playing }) => {
+	const { xScore, oScore } = scores;
+	return (
+		<div className={`scoreboard ${playing}`}>
+			<span>X - {xScore}</span>
+			<span>O - {oScore}</span>
 
-        </div>
-    )
+		</div>
+	)
 }
